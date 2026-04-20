7b5d93c5ccb046ef66af3cd7c120f17051b3c475
shihab <shihab.zn4@gmail.com>
2026-04-20 19:33:32 +0600

    feat: initialize Next.js project with Tailwind and TypeScript configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgmain: "#020617",
+        card: "#020817",
+        accent: {
+          500: "#22c55e",
+          400: "#4ade80",
+        },
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
